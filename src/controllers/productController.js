2f3b51d7cc6d7a5d37eefed941e78a0428995d1a
Alexandrey <alexandre.yucra@outlook.com>
2020-03-22 19:49:26 -0300

    finalizando o CRUD

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,10 +4,18 @@ const Product = mongoose.model('Product');
 
 module.exports = {
     async index(req, res){
+        // mostrando a listagem
         const products = await Product.find();
 
         return res.json(products);
 
+    },
+
+    async show(req, res) {
+        //mostra o dado devido ao ID
+        const product = await Product.findById(req.params.id);
+
+        return res.json(product);
     },
 
     async store(req, res) {
@@ -16,6 +24,21 @@ module.exports = {
 
         return res.json(product);
 
+    },
+
+    async update(req, res){
+        //atualiza um dado ID
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true});
+
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        //elimmina o dado ID
+        await Product.findByIdAndRemove(req.params.id);
+
+        return res.send();
     }
+
 
 };
